fa68c73ffdcaa826e31980aba902de113e20f797
Gil <gilong314@gmail.com>
2021-07-27 23:26:57 -0700

    start

--- starter/app/src/main/java/com/udacity/ButtonState.kt
+++ starter/app/src/main/java/com/udacity/ButtonState.kt
@@ -1,6 +1,9 @@
 package com.udacity
 
-
+/*
+Sealed classes and interfaces represent restricted class hierarchies that provide more control over inheritance.
+All subclasses of a sealed class are known at compile time.
+ */
 sealed class ButtonState {
     object Clicked : ButtonState()
     object Loading : ButtonState()
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +20,7 @@ import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    private val NOTIFICATION_ID = 0
 
     private var downloadID: Long = 0
 
@@ -23,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,6 +39,10 @@ class MainActivity : AppCompatActivity() {
         custom_button.setOnClickListener {
             download()
         }
+    createChannel(
+        //since the channel and send notification uses the same channel_id, the notification will be passed through said channel
+        getString(R.string.notification_id),
+        getString(R.string.notification_name))
     }
 
     private val receiver = object : BroadcastReceiver() {
@@ -40,7 +50,55 @@ class MainActivity : AppCompatActivity() {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
     }
+    //when to call this function, in a broadcast class?
+    fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+        val detailIntent = Intent(applicationContext, DetailActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            detailIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.notification_id)
+        )
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(applicationContext
+                .getString(R.string.notification_title))
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true)
+            .setContentText(messageBody)
+            .build()
+
+        notify(NOTIFICATION_ID, builder)
+    }
+
+        @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel(channelId: String, channelName: String) {
+        // TODO: Step 1.6 START create a channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                // TODO: Step 2.4 change importance
+                NotificationManager.IMPORTANCE_LOW
+            )
+            // TODO: Step 2.6 disable badges for this channel
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Download complete"
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+//The download manager is a system service that handles long-running HTTP downloads.
+//Clients may request that a URI be downloaded to a particular destination file.
     private fun download() {
         val request =
             DownloadManager.Request(Uri.parse(URL))
